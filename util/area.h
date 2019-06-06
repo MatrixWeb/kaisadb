9d9c2bd425719ae00b4a6ceeb2e1526b563a214e
matrixlin <matrixlin@tencent.com>
2019-06-06 11:46:41 +0800

    add memory allocator area head

---
+++ util/area.h
@@ -0,0 +1,40 @@
+#include<cstddef>
+#include<cstdint>
+#include<atomic>
+#include<vector>
+#ifndef STORAGE_KAISADB_AREA_H_
+
+
+#define STORAGE_KAISADB_AREA_H_
+namespace kaisadb {
+
+class Area
+{
+private:
+    /* data */
+public:
+    Area(/* args */);
+    Area(const Area&) = delete;
+    char* Allocate(size_t bytes);
+    char* AllocateAligned(size_t bytes);
+    ~Area();
+private:
+    //Allocation state
+    char* alloc_ptr_;
+    size_t alloc_size_remaining_;
+
+    std::vector<char*> blocks_;
+    std::atomic<size_t> memory_usage_;
+};
+
+Area::Area(/* args */)
+{
+}
+
+Area::~Area()
+{
+}
+
+
+}  //namespace kaisadb
+#endif
